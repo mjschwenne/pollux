d551c86ec0f92ec2d9d8ac165c61ba77121bfbf9
Matt Schwennesen <mjschwenne@gmail.com>
2025-10-28 17:49:24 -0500

    Setup parser theorems

---
+++ src/parse/Theorems.v
@@ -0,0 +1,12 @@
+From Pollux Require Import Prelude.
+From Pollux.parse Require Import Util.
+From Pollux.parse Require Import Input.
+From Pollux.parse Require Import Parser.
+
+Module ParseTheorems.
+  Section ParseTheorems.
+    Import Parser.
+
+
+  End ParseTheorems.
+End ParseTheorems.
