4719e1a005af9bd6f7c49ea66f19a597747e7b7b
Matt Schwennesen <mjschwenne@gmail.com>
2026-08-18 14:10:48 -0500

    Update aristotle cli

--- lean/aristotle.nix
+++ lean/aristotle.nix
@@ -1,7 +1,7 @@
 {
   lib,
   buildPythonPackage,
-  fetchPypi,
+  fetchurl,
   setuptools,
   pydantic,
   httpx,
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "aristotlelib";
-  version = "1.0.1";
-  pyproject = true;
+  version = "2.1.0";
+  format = "wheel";
 
-  src = fetchPypi {
-    inherit pname version;
-    hash = "sha256-FON0tngCWoCbLOJPAxD9uOo1WZFg22M51cnmKwRMlho=";
+  src = fetchurl {
+    url = "https://files.pythonhosted.org/packages/c5/63/1d25b34c331deee0e667881f81b5760d4d9c10f4f787cb8c6e0992ed5c16/aristotlelib-2.1.0-py3-none-any.whl";
+    hash = "sha256-8pDI4LdLFbW1tDLbbdeRWtyPSf6D7+8P0vLqU7XDNVU=";
   };
 
   build-system = [ setuptools ];
